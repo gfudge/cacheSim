16e6919277fa27f9a7d76ec2f3046c079ddbf6b3
gfudge <g.fudge-11@student.lboro.ac.uk>
2014-02-25 13:23:43 +0000

    Class model for cache

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 #!/usr/bin/python
 
+class Cache(cacheMemorySize, ways, cacheLineLength):
 
+	# Constructor Method
+	def __init__(self):
+		# Class Properties
+		self.memorySize = cacheMemorySize
+		self.ways = ways
+		self.lineLength = cacheLineLength
+		
+		self.blockCount = (self.memorySize)/(self.lineLength)
+		# Init blocks as hash table
+		self.cacheBlocks = {}
+		# Flush the cache
+		self.flush()
+
+	def flush(self):
+		# For all lines in cache
+		for line in range(self.memorySize):
+			# flush cache line
+			#
+
+	def access(self):
+
+	def write(self):
+		# write to cache
+
+	def read(self):
+		# read from cache
